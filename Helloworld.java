db42d2f94368d763f49e6bafe246ba9ad0cf7fe8
WYY <2367872649@qq.com>
2023-07-28 15:35:27 +0800

    提交文件

---
+++ Helloworld.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Helloworld {
+        public static void main(String[] args){
+            System.out.println("HelloWorld");
+            System.out.println("你好，世界！");
+
+
+            int i=10;
+            System.out.println(i);
+            long l=99999999L;
+            System.out.println(l);
+            float f=1.1F;
+            System.out.println(f);
+            double d=1.03;
+            System.out.println(d);
+
+        }
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+
+
+    }
